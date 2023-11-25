2f4f45c66b86a2e7e60b7c72c8752690bb128321
Imam <basstimam@gmailc.om>
2023-11-25 09:27:32 +0700

    [Update] HomeScreen beres

--- app/src/main/java/com/example/jetpackcrypto/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcrypto/MainActivity.kt
@@ -3,20 +3,35 @@ package com.example.jetpackcrypto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcrypto.model.CoinsData
 import com.example.jetpackcrypto.ui.theme.JetpackCryptoTheme
 import com.example.jetpackcrypto.ui.theme.components.list_coin.CryptoListItem
+import com.example.jetpackcrypto.ui.theme.components.scroll.ScrollToTopButton
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,15 +48,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun JetpackCryptoApp(modifier: Modifier = Modifier) {
 
-        Box(modifier = modifier)    {
+        Box(modifier = modifier.fillMaxSize())    {
+            val scope = rememberCoroutineScope()
+            val listState = rememberLazyListState()
+            val showButton: Boolean by remember {
+                derivedStateOf { listState.firstVisibleItemIndex > 0 }
+            }
 
-    LazyColumn{
+    LazyColumn(state = listState, modifier = Modifier.padding(10.dp)){
         items(CoinsData.coins, key = { it.id }) { hero ->
             CryptoListItem(name = hero.name, price = hero.price)
         }
     }
+            AnimatedVisibility(
+                visible = showButton,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically(),
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .align(Alignment.BottomCenter)
+            ) {
+                ScrollToTopButton(
+                    onClick = {
+                        scope.launch {
+                            listState.scrollToItem(index = 0)
+                        }
+                    }
+                )
+            }
 
         }
+
 
     }
 
--- app/src/main/java/com/example/jetpackcrypto/ui/theme/components/list_coin/CryptoList.kt
+++ app/src/main/java/com/example/jetpackcrypto/ui/theme/components/list_coin/CryptoList.kt
@@ -1,7 +1,12 @@
 package com.example.jetpackcrypto.ui.theme.components.list_coin
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -9,7 +14,9 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,30 +26,22 @@ fun CryptoListItem(name: String, price: Double, modifier: Modifier = Modifier)
 {
 
     Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-        ),
-        modifier = Modifier
-            .size(width = 240.dp, height = 100.dp)
-            .padding(16.dp)
-
+        modifier = Modifier.padding(10.dp).fillMaxWidth().height(70.dp),
     ) {
-        Column (modifier = modifier.padding(16.dp)){
-            Text(
-                text = name,
-                style = MaterialTheme.typography.headlineSmall,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Text(
-                text = "Price now: $$price",
-                style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                ,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = name, style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "$ $price")
         }
     }
+
 
 }
 
@@ -50,7 +49,6 @@ fun CryptoListItem(name: String, price: Double, modifier: Modifier = Modifier)
 
 @Preview (showBackground = true)
 @Composable
-
 fun DefaultPreview()
 {
     CryptoListItem(name = "Bitcoin", price = 45000.0)
